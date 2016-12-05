59399af2c7a0713972e0f1cfbf27edf9c06a665e
Yuting Zhang <tinaytz@users.noreply.github.com>
2016-12-04 23:23:57 -0800

    add ui and chat

--- public/js/register.js
+++ public/js/register.js
@@ -1,7 +1,8 @@
 var main = function() {
 	'use strict'
 
-	var url = "http://localhost:3000";
+	var url = "https://localhost:8080";
+	var redirect;
 
 	// @param userCredentials - object of user register credentials
 	function registerUser(userCredentials) {
@@ -12,12 +13,26 @@ var main = function() {
 			dataType: 'json',
 			data:	(JSON.stringify(userCredentials)),
 			success: function(response) {
-				console.log('registration passed');
+				window.location = url + response['redirect'];
+			}
+		});
+  }
+
+ // @param userCredentials - object of user login credentials
+	function loginUser(userCredentials) {
+		$.ajax({
+			type: 'POST',
+			url: url + '/login',
+			contentType: 'application/json',
+			dataType: 'json',
+			data:	(JSON.stringify(userCredentials)),
+			success: function(response) {
+				console.log(response);
 			}
 		});
 	}
 
-	$('#submit-button').click(function() {
+	$('#register-submit-button').click(function() {
 		 console.log($('.ui.form.register').form('get values'));
 		 // POSTS user information if registration form passes validation rules
 		 if($('.ui.form.register').form('is valid')) {
@@ -25,6 +40,17 @@ var main = function() {
 		 	registerUser($('.ui.form.register').form('get values'));
 		 } else {
 		 	console.log('The registration form fields are invalid');
+		 }
+	});
+
+	$('#login-submit-button').click(function() {
+		 console.log($('.ui.form.login').form('get values'));
+		 // POSTS user information if registration form passes validation rules
+		 if($('.ui.form.login').form('is valid')) {
+			console.log('The login credentials are VALID');
+			loginUser($('.ui.form.login').form('get values'));
+		 } else {
+			console.log('The login credentials are INVALID');
 		 }
 	});
 
@@ -56,7 +82,7 @@ var main = function() {
 	      		identifier : 'email',
 	      		rules: [
 	      			{
-		      			type : 'email',
+		      			type : 'email' && 'empty',
 		      			prompt : 'Please enter a valid email'
 	      			}
 	      		]
@@ -65,7 +91,7 @@ var main = function() {
 	      		identifier : 'confirm email',
 	      		rules: [
 	      			{
-		      			type : 'match[email]',
+		      			type : 'match[email]' && 'empty',
 		      			prompt : 'Emails must match'
 	      			}
 	      		]
@@ -83,8 +109,32 @@ var main = function() {
 	      		identifier : 'confirm password',
 	      		rules: [
 	      			{
-		      			type : 'match[password]',
+		      			type : 'match[password]' && 'empty',
 		      			prompt : 'Passwords must match'
+	      			}
+	      		]
+	      	},
+	    }
+	})
+
+	//Login Validation
+	$('.ui.form.login').form({
+	    fields: {
+	    	username: {
+	      		identifier : 'username',
+	      		rules: [
+	      			{
+		      			type : 'empty',
+		      			prompt : 'Please enter a username'
+	      			}
+	      		]
+	      	},
+	      	password: {
+	      		identifier : 'password',
+	      		rules: [
+	      			{
+		      			type : 'empty',
+		      			prompt : 'Please enter your password'
 	      			}
 	      		]
 	      	},
--- server.js
+++ server.js
@@ -5,23 +5,40 @@
 var express = require('express');
 var https = require('https');
 var multidep = require('multidep')('multidep.json');
-var socketio = multidep('socket.io', '0.9.17');
-var newsocketio = multidep('socket.io', '1.7.1');
 var fs = require('fs');
 var events = require('events');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var expressValidator = require('express-validator');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+var bodyParser = require('body-parser');
 var port = 8080;
 var ssldir = 'node_modules/socketio-over-nodejs/fake-keys'
 var options = {
    key: fs.readFileSync(ssldir + '/privatekey.pem'),
    cert: fs.readFileSync(ssldir + '/certificate.pem')
 };
+var socketio = multidep('socket.io', '0.9.17');
+
+var stat = require('node-static');
+var path = require('path');
 
 var app = express();
-app.use('/', express.static('public'))
-var server = https.createServer(options, app);
+var nicknames = {};
+
+// Body Parser for JSON
+app.use(bodyParser.json());
+
+var server = https.createServer(options,app);
+app.use('/', express.static('public'));
+
+var file = new stat.Server(path.join(__dirname, '..', 'public'));
+
+  function handler(req, res) {
+    file.serve(req, res);
+  }
 
 // Connect to the database
 mongoose.connect('mongodb://project2:project2@ds113668.mlab.com:13668/473project2');
@@ -31,12 +48,106 @@ db.once('open', function() {
     console.log("Connected to mlab Project 2 Database");
 });
 
+//Express Validator - Taken from express-validator documentation
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Register User - gets data from the user end and validates it
+app.post('/register', function(req, res) {
+  var email = req.body.email,
+        username = req.body.username,
+          password = req.body.password,
+          password2 = req.body.password2; // For confirm password field
+
+      // Validation using express-validator
+        req.checkBody('email', 'Invalid email').isEmail();
+        req.checkBody('username', 'Invalid username').notEmpty();
+        req.checkBody('password', 'Invalid password').notEmpty();
+        req.checkBody('password2', 'Invalid password').equals(req.body.password);
+
+        // Test for errors example - delete the console logs later
+        var errors = req.validationErrors();
+
+        // Re-render the register page with the errors
+        if(errors) {
+      res.send('errors in register validation');
+      console.log('errors in registering' + errors);
+        } else {
+            console.log('Registration passed for email: ' + email + 'name: ' + username);
+            //validation passed so lets create our new user
+            var newUser = new User( {
+                email: email,
+              username: username,
+                password: password
+            });
+
+      // Create our user - throw an error if it fails
+            User.createUser(newUser, function(err, user) {
+                if(err) throw err;
+                    console.log(user);
+            });
+        }
+});
+
+// Gets user name, checks for match in our db, validates the password
+// Functions comparePassword & getUserByUsername found in models/user.js
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.getUserByUsername(username), function(err, user) {
+        if(err) throw err;
+
+        // Is there a user in our db?
+        if(!user) {
+            return done(null, false, {message: 'No User Found'});
+        }
+
+      // Does password match?
+        User.comparePassword(password, user.password, function(err, isMatch) {
+            if(err) throw err;
+            if(isMatch) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false, {message: 'Invalid Password'});
+            }
+        });
+    };
+    }));
+
+// Passport serialization
+//Taken from official passport docs - getUserById found in our schema
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.getUserbyId(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
+
+//io.set('transports', [ /*'websocket',*/ 'xhr-polling', 'jsonp-polling' ]);
+//video
 var io = socketio.listen(server, {
     log: true,
     origins: '*:*'
 });
-
-//io.set('transports', [ /*'websocket',*/ 'xhr-polling', 'jsonp-polling' ]);
 
 var channels = {};
 
@@ -45,6 +156,8 @@ io.sockets.on('connection', function (socket) {
     if (!io.isConnected) {
         io.isConnected = true;
     }
+
+
 
     socket.on('new-channel', function (data) {
         if (!channels[data.channel]) {
@@ -60,12 +173,48 @@ io.sockets.on('connection', function (socket) {
         socket.emit('presence', isChannelPresent);
     });
 
+
+
     socket.on('disconnect', function (channel) {
         if (initiatorChannel) {
             delete channels[initiatorChannel];
         }
+         if (!socket.nickname) {
+
+        return;
+       }
+      delete nicknames[socket.nickname];
+      socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+      socket.broadcast.emit('nicknames', nicknames);
+        });
+
+//chat 
+    socket.on('nickname', function (nick, fn) {
+      console.log("get nick",nick);
+      if (nicknames[nick]) {
+        fn(true);
+      }
+      else {
+        fn(false);
+        nicknames[nick] = socket.nickname = nick;
+        socket.broadcast.emit('announcement', nick + ' connected');
+        io.sockets.emit('nicknames', nicknames);
+      }
     });
+     socket.on('user message', function (msg) {
+      socket.broadcast.emit('user message', socket.nickname, msg);
+    });
+
+    socket.on('user image', function (msg) {
+      console.log(msg);
+      socket.broadcast.emit('user image', socket.nickname, msg);
+    });
+
 });
+
+
+ 
+
 
 function onNewNamespace(channel, sender) {
     io.of('/' + channel).on('connection', function (socket) {
